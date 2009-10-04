5cecb274ac7cad4a906755a1a23e0c482282338d
Alex Morega <alex@grep.ro>
2009-10-04 13:11:35 +0300

    basic file read test

--- gitar/persistence.py
+++ gitar/persistence.py
@@ -1,3 +1,33 @@
+import gitlib
+
 class Repo(object):
     def __init__(self, repo_path):
-        self.repo_path = repo_path
+        self.git = gitlib.Repository(repo_path)
+
+    @property
+    def _current_tree(self):
+        head = self.git.find_head()
+        commit = self.git.getcommit(head)
+        tree = self.git.gettree(commit.tree)
+        return tree
+
+    def get_file(self, file_path):
+        dir_path, sep, file_name = ('data'+file_path).rpartition('/')
+
+        tree = self._current_tree
+        for dir_name in dir_path.split('/'):
+            dir_entry = dict(tree.entries())[dir_name]
+            assert dir_entry.is_dir()
+            tree = self.git.gettree(dir_entry.name)
+
+        file_entry = dict(tree.entries())[file_name]
+        assert not file_entry.is_dir()
+        file_blob = self.git.getblob(file_entry.name)
+        file_data = file_blob.text
+
+        return RepoFile(file_name, file_data)
+
+class RepoFile(object):
+    def __init__(self, name, data):
+        self.name = name
+        self.data = data
--- gitar/tests/read_git.py
+++ gitar/tests/read_git.py
@@ -12,10 +12,15 @@ class ReadGitTestCase(unittest.TestCase):
         self.tmpdir = tempfile.mkdtemp()
         self.repodir = path.join(self.tmpdir, path.basename(test_git_path))
         shutil.copytree(test_git_path, self.repodir)
-        self.repo = Repo(self.tmpdir)
+        self.repo = Repo(self.repodir)
 
     def tearDown(self):
         shutil.rmtree(self.tmpdir)
+
+    def test_get_file(self):
+        a = self.repo.get_file('/a.txt')
+        self.assertEqual(a.name, 'a.txt')
+        self.assertEqual(a.data, 'text file "a"\n')
 
 if __name__ == '__main__':
     unittest.main()
