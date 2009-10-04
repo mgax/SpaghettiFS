0ca2aa3c084bb0abdf881cd42fd6b2897061ac12
Alex Morega <alex@grep.ro>
2009-10-04 17:00:04 +0300

    ported git-related code to use the `dulwich` library

--- gitar/persistence.py
+++ gitar/persistence.py
@@ -1,22 +1,21 @@
-import gitlib
+import dulwich
 
 class Repo(object):
     def __init__(self, repo_path):
-        self.git = gitlib.Repository(repo_path)
+        self.git = dulwich.repo.Repo(repo_path)
 
     @property
     def _current_tree(self):
-        head = self.git.find_head()
-        commit = self.git.getcommit(head)
-        tree = self.git.gettree(commit.tree)
+        head = self.git.head()
+        commit = self.git.commit(head)
+        tree = self.git.tree(commit.tree)
         return tree
 
     def _get_dir(self, dir_path):
         dir_tree = self._current_tree
         for dir_name in dir_path.split('/'):
-            dir_entry = dict(dir_tree.entries())[dir_name]
-            assert dir_entry.is_dir()
-            dir_tree = self.git.gettree(dir_entry.name)
+            dir_entry = dict((i[0], i[2]) for i in dir_tree.iteritems())[dir_name]
+            dir_tree = self.git.tree(dir_entry)
 
         return dir_tree
 
@@ -25,17 +24,16 @@ class Repo(object):
         dir_path, sep, file_name = ('data'+file_path).rpartition('/')
 
         dir_tree = self._get_dir(dir_path)
-        file_entry = dict(dir_tree.entries())[file_name]
-        assert not file_entry.is_dir()
-        file_blob = self.git.getblob(file_entry.name)
-        file_data = file_blob.text
+        file_entry = dict((i[0], i[2]) for i in dir_tree.iteritems())[file_name]
+        file_blob = self.git.get_blob(file_entry)
+        file_data = file_blob.data
 
         return RepoFile(file_name, file_data)
 
     def list_files(self, dir_path):
         assert dir_path.startswith('/'), dir_path.endswith('/')
         dir_tree = self._get_dir('data' + dir_path[:-1])
-        return [entry[0] for entry in dir_tree.entries()]
+        return [entry[1] for entry in dir_tree.entries()]
 
 class RepoFile(object):
     def __init__(self, name, data):
