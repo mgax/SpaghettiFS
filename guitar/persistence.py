61ef6125912e61a01d996a7d56ab0ca07f20c7c3
Alex Morega <alex@grep.ro>
2009-10-04 23:45:26 +0300

    'path' property for backend objects

--- guitar/persistence.py
+++ guitar/persistence.py
@@ -7,19 +7,24 @@ class Repo(object):
 
     def get_root(self):
         git_id = self.git.commit(self.git.head()).tree
-        commit_tree = RepoDir(self.git, git_id, None)
+        commit_tree = RepoDir(self.git, git_id, None, None)
         root_tree = commit_tree['data']
         root_tree.name = '[root]'
+        root_tree.path = '/'
         return root_tree
 
 class RepoDir(UserDict.DictMixin):
     is_dir = True
 
-    def __init__(self, git, git_id, name):
+    def __init__(self, git, git_id, name, parent):
         self.git = git
         self.git_id = git_id
         self.name = name
-        #self.items = [item[0] for item in self.tree.iteritems()]
+        self.parent = parent
+
+    @property
+    def path(self):
+        return self.parent.path + self.name + '/'
 
     def itertree(self):
         return self.git.tree(self.git_id).iteritems()
@@ -28,22 +33,26 @@ class RepoDir(UserDict.DictMixin):
         for name, mode, git_id in self.itertree():
             if name == key:
                 if mode == 16384: # directory
-                    return RepoDir(self.git, git_id, name)
+                    return RepoDir(self.git, git_id, name, self)
                 else: # regular file
-                    return RepoFile(self.git, git_id, name)
+                    return RepoFile(self.git, git_id, name, self)
         raise KeyError(key)
 
     def keys(self):
         return [name for (name, mode, git_id) in self.itertree()]
 
-
 class RepoFile(object):
     is_dir = False
 
-    def __init__(self, git, git_id, name):
+    def __init__(self, git, git_id, name, parent):
         self.git = git
         self.git_id = git_id
         self.name = name
+        self.parent = parent
         self.blob = self.git.get_blob(git_id)
         self.data = self.blob.data
         self.size = len(self.blob.data)
+
+    @property
+    def path(self):
+        return self.parent.path + self.name
--- guitar/tests/read_git.py
+++ guitar/tests/read_git.py
@@ -2,7 +2,7 @@ import unittest
 from support import GuitarTestCase
 
 
-class ReadGitTestCase(GuitarTestCase):
+class BackendTestCase(GuitarTestCase):
     def test_walk(self):
         root = self.repo.get_root()
         self.assertTrue(root.is_dir)
@@ -11,16 +11,23 @@ class ReadGitTestCase(GuitarTestCase):
 
         a_txt = root['a.txt']
         self.assertFalse(a_txt.is_dir)
+        self.assertEqual(a_txt.name, 'a.txt')
         self.assertEqual(a_txt.size, 14)
         self.assertEqual(a_txt.data, 'text file "a"\n')
+        self.assertEqual(a_txt.path, '/a.txt')
 
         b = root['b']
         self.assertTrue(b.is_dir)
         self.assertEqual(set(b.keys()), set(['c', 'f.txt']))
+        self.assertEqual(b.path, '/b/')
 
         c = b['c']
         self.assertTrue(c.is_dir)
         self.assertEqual(set(c.keys()), set(['d.txt', 'e.txt']))
+        self.assertEqual(c.path, '/b/c/')
+
+        d = c['d.txt']
+        self.assertEqual(d.path, '/b/c/d.txt')
 
 if __name__ == '__main__':
     unittest.main()
