14c3965f2270fd1d22983c55e4a0cab172ac7ce9
Alex Morega <alex@grep.ro>
2009-10-05 23:27:00 +0300

    basic write support - create empty files

--- guitar/storage.py
+++ guitar/storage.py
@@ -1,17 +1,34 @@
+from time import time
 import UserDict
 import dulwich
 
 class Repo(object):
     def __init__(self, repo_path):
         self.git = dulwich.repo.Repo(repo_path)
+        self.tree_git_id = self.git.commit(self.git.head()).tree
 
     def get_root(self):
-        git_id = self.git.commit(self.git.head()).tree
-        commit_tree = RepoDir(self.git, git_id, None, None)
-        root_tree = commit_tree['data']
-        root_tree.name = '[root]'
+        commit_tree = self.git.tree(self.tree_git_id)
+        root_id = dict((i[0], i[2]) for i in commit_tree.iteritems())['data']
+        root_tree = RepoDir(self.git, root_id, '[root]', self)
         root_tree.path = '/'
         return root_tree
+
+    def update(self, mode, name, git_id, msg):
+        commit_tree = self.git.tree(self.tree_git_id)
+        commit_tree.add(040000, 'data', git_id)
+        self.git.object_store.add_object(commit_tree)
+        self.tree_git_id = commit_tree.id
+
+        commit = dulwich.objects.Commit()
+        commit.tree = self.tree_git_id
+        commit.author = commit.committer = "Guitar User <noreply@grep.ro>"
+        commit.commit_time = commit.author_time = int(time())
+        commit.commit_timezone = commit.author_timezone = 2*60*60
+        commit.encoding = "UTF-8"
+        commit.message = "Auto commit: %s" % msg
+        self.git.object_store.add_object(commit)
+        self.git.refs['refs/heads/master'] = commit.id
 
 class RepoDir(UserDict.DictMixin):
     is_dir = True
@@ -32,7 +49,7 @@ class RepoDir(UserDict.DictMixin):
     def __getitem__(self, key):
         for name, mode, git_id in self.itertree():
             if name == key:
-                if mode == 16384: # directory
+                if mode == 040000: # directory
                     return RepoDir(self.git, git_id, name, self)
                 else: # regular file
                     return RepoFile(self.git, git_id, name, self)
@@ -40,6 +57,21 @@ class RepoDir(UserDict.DictMixin):
 
     def keys(self):
         return [name for (name, mode, git_id) in self.itertree()]
+
+    def update(self, mode, name, git_id, msg):
+        tree = self.git.tree(self.git_id)
+        tree.add(mode, name, git_id)
+        self.git.object_store.add_object(tree)
+        self.git_id = tree.id
+        self.parent.update(040000, self.name, self.git_id, msg)
+
+    def create_file(self, name):
+        # TODO: check filename
+        blob = dulwich.objects.Blob.from_string('')
+        self.git.object_store.add_object(blob)
+        msg = "creating file %s" % (self.path + name)
+        self.update(0100644, name, blob.id, msg)
+        return self[name]
 
 class RepoFile(object):
     is_dir = False
--- guitar/tests/test_storage.py
+++ guitar/tests/test_storage.py
@@ -29,5 +29,21 @@ class BackendTestCase(GuitarTestCase):
         d = c['d.txt']
         self.assertEqual(d.path, '/b/c/d.txt')
 
+    def test_create_file(self):
+        b = self.repo.get_root()['b']
+        g_txt = b.create_file('g.txt')
+        self.assertFalse(g_txt.is_dir)
+        self.assertEqual(g_txt.size, 0)
+        self.assertEqual(g_txt.data, '')
+        self.assertEqual(g_txt.name, 'g.txt')
+
+        from guitar.storage import Repo
+        repo2 = Repo(self.repo_path)
+        g_txt_2 = repo2.get_root()['b']['g.txt']
+        self.assertFalse(g_txt_2.is_dir)
+        self.assertEqual(g_txt_2.size, 0)
+        self.assertEqual(g_txt_2.data, '')
+        self.assertEqual(g_txt_2.name, 'g.txt')
+
 if __name__ == '__main__':
     unittest.main()
