975dc08d6ec0fdcd65225e6a99a7de7cf9350f0e
Alex Morega <alex@grep.ro>
2009-10-12 23:15:32 +0300

    created setup.py; moved bugs to github tracker

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "SpaghettiFS",
+    version = "0.1",
+    packages = find_packages(),
+    install_requires = ['dulwich>=0.3.3'],
+    author = "Alex Morega",
+    author_email = "spaghettifs@grep.ro",
+    description = "Git-backed FUSE filesystem",
+    license = "BSD License",
+    keywords = "hello world example examples",
+    url = "http://github.com/alex-morega/spaghettifs",
+)
