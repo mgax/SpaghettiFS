3670fa27d673685f3c0b91aa4adba7bcf3d4aa93
Alex Morega <alex@grep.ro>
2009-10-13 23:41:35 +0300

    basic implementation of tree and commit

--- spaghettifs/easygit.py
+++ spaghettifs/easygit.py
@@ -1,7 +1,59 @@
+from time import time
+
 import dulwich
 
+class EasyTree(object):
+    _git_tree = None
+
+    def __init__(self, git_repo, git_id=None):
+        self.git = git_repo
+        if git_id is None:
+            git_tree = dulwich.objects.Tree()
+            self.git.object_store.add_object(git_tree)
+            git_id = git_tree.id
+        self.git_id = git_id
+
+    def __enter__(self):
+        assert self._git_tree is None
+        self._git_tree = dulwich.objects.Tree()
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        assert self._git_tree is not None
+        self.git.object_store.add_object(self._git_tree)
+        self.git_id = self._git_tree.id
+        del self._git_tree
+
+    def __setitem__(self, key, value):
+        assert self._git_tree is not None
+        assert value._git_tree is None
+        self._git_tree[key] = (040000, value.git_id)
+
 class EasyGit(object):
+    def __init__(self, git_repo):
+        self.git = git_repo
+
+    def new_tree(self):
+        return EasyTree(self.git)
+
+    def commit(self, author, message, tree):
+        commit_time = int(time())
+
+        git_commit = dulwich.objects.Commit()
+        git_commit.commit_time = commit_time
+        git_commit.author_time = commit_time
+        git_commit.commit_timezone = 2*60*60
+        git_commit.author_timezone = 2*60*60
+        git_commit.author = author
+        git_commit.committer = author
+        git_commit.message = message
+        git_commit.encoding = "UTF-8"
+        git_commit.tree = tree.git_id
+
+        self.git.object_store.add_object(git_commit)
+        self.git.refs['refs/heads/master'] = git_commit.id
+
     @classmethod
     def new(cls, repo_path, bare=False):
         assert bare is True
-        repo = dulwich.repo.Repo.init_bare(repo_path)
+        git_repo = dulwich.repo.Repo.init_bare(repo_path)
+        return cls(git_repo)
--- spaghettifs/tests/test_easygit.py
+++ spaghettifs/tests/test_easygit.py
@@ -3,6 +3,7 @@ import tempfile
 import shutil
 import os
 
+import dulwich
 from spaghettifs.easygit import EasyGit
 
 class EasyGitTestCase(unittest.TestCase):
@@ -18,12 +19,30 @@ class EasyGitTestCase(unittest.TestCase):
         self.assertTrue(set(os.listdir(self.repo_path)), set(expected_items))
 
     def test_initial_commit(self):
-        t = self.eg.new_tree()
-        c = self.eg.new_commit()
-        c.tree = t
-        c.author = "Spaghetti User <noreply@grep.ro>"
-        c.message = "initial test commit"
-        c.save()
+        self.eg.commit(author="Spaghetti User <noreply@grep.ro>",
+                       message="initial test commit",
+                       tree=self.eg.new_tree())
+
+        git = dulwich.repo.Repo(self.repo_path)
+        git_h = git.head()
+        git_c = git.commit(git_h)
+        self.assertEqual(git_c.author, "Spaghetti User <noreply@grep.ro>")
+        self.assertEqual(git_c.message, "initial test commit")
+        self.assertEqual(git_c.get_parents(), [])
+
+    def test_commit_with_tree(self):
+        t1 = self.eg.new_tree()
+        t2 = self.eg.new_tree()
+        with t1:
+            t1['t2'] = t2
+        self.eg.commit(author="Spaghetti User <noreply@grep.ro>",
+                       message="test commit with tree",
+                       tree=t1)
+
+        git = dulwich.repo.Repo(self.repo_path)
+        git_t = git.tree(git.commit(git.head()).tree)
+        self.assertEqual(len(git_t.entries()), 1)
+        self.assertEqual(git_t.entries()[0][:2], (040000, 't2'))
 
 if __name__ == '__main__':
     unittest.main()
