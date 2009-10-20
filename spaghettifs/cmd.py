35c1f93e1f7ccee6cd344d1ebb5f64ae9c3dbd88
Alex Morega <alex@grep.ro>
2009-10-20 23:30:49 +0300

    updated cmdline script and README for mkfs support

--- spaghettifs/cmd.py
+++ spaghettifs/cmd.py
@@ -2,9 +2,11 @@ import sys
 import logging
 from optparse import OptionParser
 
+from spaghettifs.storage import GitStorage
 from spaghettifs.filesystem import mount
 
-parser = OptionParser()
+usage = "usage: %prog <mkfs REPO_PATH | mount REPO_PATH MOUNT_PATH [options]>"
+parser = OptionParser(usage=usage)
 parser.add_option("-v", "--verbose",
                   action="store_const", const=logging.DEBUG, dest="loglevel")
 parser.add_option("-q", "--quiet",
@@ -13,10 +15,24 @@ parser.set_defaults(loglevel=logging.INFO)
 
 def main():
     options, args = parser.parse_args()
-    repo_path, mount_path = args
 
-    print "mounting %r at %r" % (repo_path, mount_path)
-    mount(repo_path, mount_path, loglevel=options.loglevel)
+    if not args:
+        return parser.print_usage()
+
+    elif args[0] == 'mkfs':
+        if len(args) != 2:
+            return parser.print_usage()
+        GitStorage.create(args[1])
+
+    elif args[0] == 'mount':
+        if len(args) != 3:
+            return parser.print_usage()
+        repo_path, mount_path = args[1:]
+        print "mounting %r at %r" % (repo_path, mount_path)
+        mount(repo_path, mount_path, loglevel=options.loglevel)
+
+    else:
+        return parser.print_usage()
 
 if __name__ == '__main__':
     main()
